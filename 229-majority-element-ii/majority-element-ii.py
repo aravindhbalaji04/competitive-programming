77416bad09fd1c5d7f1b1bb44b603d44931db576
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2023-10-05 12:57:44 +0530

    Time: 110 ms (62.72%) | Memory: 17.7 MB (49.91%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,12 +1,3 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
-        y = []
-        z = []
-        for i in range(len(nums)):
-            a = nums.count(nums[i])
-            if a > len(nums)/3:
-                y.append(nums[i])
-        for i in range(len(y)):
-            if y[i] not in z:
-                z.append(y[i])
-        return z
+    def majorityElement(self, nums):
+        return [num for num, count in Counter(nums).items() if count > len(nums) // 3]
