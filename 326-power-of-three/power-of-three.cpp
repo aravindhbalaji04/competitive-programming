02aac3ebc01242ef59fb24f16192da9bcb8a1a18
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2023-10-23 12:29:28 +0530

    Time: 20 ms (21.30%) | Memory: 6.2 MB (49.65%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,11 +1,6 @@
-unordered_set<int> s;
-bool f = []{
-    for (uint i = 0, powOf3 = 1; i <= 19; i++, powOf3 *= 3) s.insert(powOf3);    
-    return true;
-}(); 
 class Solution {
-public:    
+public:
     bool isPowerOfThree(int n) {
-        return s.find(n) != end(s);
+        return n > 0 && 1162261467 % n == 0;
     }
 };
