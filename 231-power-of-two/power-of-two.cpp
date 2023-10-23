9aab5fe3dc27c6b0dc7f07c610b4a84aee9f11d4
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2023-10-23 12:27:45 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (48.27%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
-        
-        if(n == 0) return false;
-        if(n == 1) return true;
-
-        while((n % 2) == 0)
-        {
-            n = n / 2;
-        }
-
-        return n == 1;
+        return n && !(n & (n-1l));
     }
 };
