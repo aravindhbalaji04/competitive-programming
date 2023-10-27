231faa1bbc36c1f0fda0a1be5306bc32a4989042
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2023-10-27 10:53:00 +0530

    Time: 65 ms (18.22%) | Memory: 19.6 MB (15.05%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -0,0 +1,15 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        if not s: return s
+        s = list(s)
+        s.sort()
+        all_strings = []
+        cur_sb = [s[0]]
+        for c in s[1:]:
+            if cur_sb[-1] != c:
+                all_strings.append("".join(cur_sb))
+                cur_sb = []
+            cur_sb.append(c)
+        all_strings.append("".join(cur_sb))
+        all_strings.sort(key=lambda string : len(string), reverse=True)
+        return "".join(all_strings)
