26122aa31cc54c80ee367f914e475a49329e6689
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2023-10-31 13:27:29 +0530

    Time: 69 ms (99.12%) | Memory: 16.2 MB (93.24%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,20 +1,44 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        hmap = defaultdict(int)
-        n,res=len(nums),[]
-        for i in range(0,n):
-            if(i>0 and nums[i-1]==nums[i]):
-                continue;
-            for j in range(i+1,n):
-                for k in range(j+1,n):   
-                    t = target-(nums[i]+nums[j]+nums[k])
-                    idx = bisect.bisect_left(nums[k+1:n],t)
-                    if( k+idx+1 < n and nums[k+idx+1]== t ):
-                        lis = [nums[i],nums[j],nums[k],nums[k+idx+1]]
-                        temp = ''.join(map(str,lis))
-                        if(hmap[temp] == 0 ):
-                            res.append(lis)
-                            hmap[temp] = 1
-        print(res)
+        i = 0
+        L = len(nums)
+        res = []
+        while i < L-3:
+            if target-nums[i] < 3*nums[i+1] or target-nums[i] > 3*nums[-1]:
+                    while i < L-4 and nums[i] == nums[i+1]:
+                        i+=1
+                    i+=1
+                    continue
+            j = i+1
+            while j < L-2:
+                if target-nums[i]-nums[j] < 2*nums[j+1] or target-nums[i]-nums[j] > 2*nums[-1]:
+                    while j < L-3 and nums[j] == nums[j+1]:
+                        j+=1
+                    j+=1
+                    continue
+                left = j+1
+                right = L-1
+                new_target = target-nums[i]-nums[j]
+                while left<right:
+                    if nums[left]+nums[right] > new_target:
+                        right-=1
+                    elif nums[left]+nums[right] < new_target:
+                        left+=1
+                    else:
+                        res.append([nums[i],nums[j],nums[left],nums[right]])
+                        temp_left = nums[left]
+                        temp_right = nums[right]
+                        while(left<right and nums[left]==temp_left):
+                            left+=1
+                        while(left<right and nums[right]==temp_right):
+                            right-=1
+                while j < L-3 and nums[j] == nums[j+1]:
+                    j+=1
+                j+=1
+            while i < L-4 and nums[i] == nums[i+1]:
+                i+=1
+            i+=1
         return res
+
+        
