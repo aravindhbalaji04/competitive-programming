ad1d58dff1257ab148f1e46bde7cabb49901580c
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2023-11-03 10:28:16 +0530

    Time: 186 ms (52.82%) | Memory: 27.5 MB (95.22%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,4 +1,4 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        break_point = (len(nums) - k) % len(nums)
-        nums[:] = nums[break_point:] + nums[:break_point]
+        k%=len(nums)
+        nums[:]=nums[-k:]+nums[:-k]
