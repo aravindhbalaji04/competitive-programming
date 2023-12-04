de005ea16af02bd298e2e20fb5f3a7778f5a345f
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2023-12-04 07:11:23 +0530

    Time: 41 ms (67.89%) | Memory: 16.4 MB (5.11%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -4,10 +4,10 @@ class Solution:
         for i in range(2,len(num)):
             if num[i-2] == num[i-1] == num[i]:
                 arr.append(int(num[i]*3))
-        if len(arr) > 0:
+        try:
             if max(arr) == 0:
                 return "000"
             else:
                 return str(max(arr))
-        else:
+        except:
             return ""
