7c565a6a6745aaed77a428fcb4accf67048df7e2
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2023-12-08 21:01:41 +0530

    Time: 633 ms (20.89%) | Memory: 30.4 MB (60.99%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -3,17 +3,13 @@ class Solution:
         x = 0
         for i in range(len(nums)):
             if nums[i] > 0:
-                x += 1
-                break
-        if x > 0:
-            best, total = 0, 0
-            for i in range(len(nums)):
-                total = max(nums[i],total+nums[i])
-                best = max(best,total)
-            return best
-        else:
-            best, total = nums[0], nums[0]
-            for i in range(len(nums)):
-                total = max(nums[i],total+nums[i])
-                best = max(best,total)
-            return best
+                best, total = 0, 0
+                for i in range(len(nums)):
+                    total = max(nums[i],total+nums[i])
+                    best = max(best,total)
+                return best
+        best, total = nums[0], nums[0]
+        for i in range(len(nums)):
+            total = max(nums[i],total+nums[i])
+            best = max(best,total)
+        return best
