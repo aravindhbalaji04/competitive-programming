09e7450fd8e12ca6214abe7b5e9507f926acab24
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2023-12-08 21:26:02 +0530

    Time: 79 ms (60.83%) | Memory: 17 MB (18.14%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -5,7 +5,6 @@ class Solution:
         for i in nums:
             x = (i,i*curr_min,i*curr_max)
             curr_min,curr_max=min(x),max(x)
-
             ans = max(ans,curr_max)
         return ans
     
