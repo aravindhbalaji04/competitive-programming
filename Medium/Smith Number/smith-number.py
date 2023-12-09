3900de6b932a4d8d0cf8d1eabb71af7628fb1bab
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2023-12-09 09:06:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Smith Number/smith-number.py
@@ -0,0 +1,39 @@
+class Solution:
+    def smithNum(self, n):
+        s1=Solution.total(n)
+        s2=0
+        i=2
+        count=0
+        while n!=1:
+            if n%i==0 and i<=9:
+                s2+=i
+                n=n//i
+                count+=1
+            elif n%i==0 and i>9:
+                s2+=Solution.total(i)
+                n=n//i
+                count+=1
+            else:
+                i+=1
+        if (s1==s2) and count>1 :
+            return 1
+        return 0
+    def total(n):
+        sm=0
+        while n!=0:
+            sm+=n%10
+            n=n//10
+        return sm
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n=int(input())
+        
+        ob = Solution()
+        print(ob.smithNum(n))
+# } Driver Code Ends
