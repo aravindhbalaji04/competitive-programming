339b96bae8876173776d8f74d42fd91ce1e28b1f
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-01-15 15:09:59 +0530

    Time: 1404 ms (88.98%) | Memory: 72.1 MB (35.64%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -3,12 +3,9 @@ class Solution:
         zero_loss = set()
         one_loss = set()
         more_losses = set()
-        
         for winner, loser in matches:
-            # Add winner
             if (winner not in one_loss) and (winner not in more_losses):
                 zero_loss.add(winner)
-            # Add or move loser.
             if loser in zero_loss:
                 zero_loss.remove(loser)
                 one_loss.add(loser)
@@ -19,5 +16,4 @@ class Solution:
                 continue
             else:
                 one_loss.add(loser)          
-            
         return [sorted(list(zero_loss)), sorted(list(one_loss))]
