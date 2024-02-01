5872a69cce84ae27a3bf28a8550a066048045abf
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-02-01 22:34:49 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Panagram Checking/panagram-checking.py
@@ -0,0 +1,36 @@
+class Solution:
+    def checkPangram(self,s):
+        asc=s.upper()
+        for i in range(65,91):
+            if chr(i) not in asc:
+                return 0
+        return 1
+        
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s=str(input())
+        obj = Solution()
+        if(obj.checkPangram(s)):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
