87fa955be83f4ab2dad855ec6cdac441c23c14d2
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-06-29 09:08:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Identical Linked Lists/identical-linked-lists.py
@@ -0,0 +1,62 @@
+def areIdentical(head1, head2):
+    current1 = head1
+    current2 = head2
+    while current1 !=None and current2!=None:
+        if(current1.data != current2.data):
+            return False
+        current1=current1.next
+        current2 = current2.next
+    return (current1 ==None and current2==None)
+
+#{ 
+ # Driver Code Starts
+# Node Class
+class node:
+
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+
+# Linked List Class
+class Linked_List:
+
+    def __init__(self):
+        self.head = None
+
+    def insert(self, val):
+        new_node = node(val)
+        new_node.data = val
+        new_node.next = self.head
+        self.head = new_node
+
+
+def printList(head):
+    while head:
+        print(head.data, end=' ')
+        head = head.next
+    print()
+
+
+def createList(arr, n):
+    lis = Linked_List()
+    for i in range(n):
+        lis.insert(arr[i])
+    return lis.head
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        head1 = createList(arr, n)
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        head2 = createList(arr, n)
+        if (areIdentical(head1, head2)):
+            print('true')
+        else:
+            print('false')
+
+# } Driver Code Ends
