debed015367a5551bf67603888202d41ce9a786e
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-07-09 06:18:10 +0530

    Time: 764 ms (30.93%) | Memory: 56.9 MB (95.53%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.py
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -1,9 +1,8 @@
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
-        next_idle_time = 0
-        net_wait_time = 0
+        idle = 0
+        wait = 0
         for customer in customers:
-            next_idle_time = max(customer[0], next_idle_time) + customer[1]
-            net_wait_time += next_idle_time - customer[0]
-        average_wait_time = net_wait_time / len(customers)
-        return average_wait_time
+            idle = max(customer[0], idle) + customer[1]
+            wait += idle - customer[0]
+        return wait / len(customers)
