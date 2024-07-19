b77ee24af93d71e1eb2aea194a4c540b7536bd58
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-07-19 22:18:26 +0530

    Time: 38 ms (65.63%) | Memory: 16.5 MB (36.65%) - LeetSync

--- 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.py
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.py
@@ -1,9 +1,3 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        arr = []
-        total = 0
-        for i in range(n):
-            arr.append((2*i)+1)
-        for i in range(n):
-            total += abs(arr[i]-arr[n//2])
-        return total//2
+        return round((n**2-1)/4)
