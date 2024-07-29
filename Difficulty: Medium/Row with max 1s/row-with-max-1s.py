7a5dbd218c3572c3314a570c70fd50a5bdb4b0bd
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-07-29 18:56:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.py
@@ -0,0 +1,30 @@
+class Solution:
+	def rowWithMax1s(self,arr):
+		ans = []
+		for i in range(len(arr)):
+		    ans.append(sum(arr[i]))
+		if max(ans) == 0:
+		    return -1
+		return ans.index(max(ans))
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, m = list(map(int, input().strip().split()))
+
+        inputLine = list(map(int, input().strip().split()))
+        arr = [[0 for j in range(m)] for i in range(n)]
+
+        for i in range(n):
+            for j in range(m):
+                arr[i][j] = inputLine[i * m + j]
+        ob = Solution()
+        ans = ob.rowWithMax1s(arr)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
