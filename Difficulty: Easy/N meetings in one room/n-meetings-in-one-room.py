362d056c9a928c1e582f9f91ec8cb44eb54a883e
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-08-04 07:57:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maximumMeetings(self,n,start,end):
+        meetings = list(zip(start, end))
+        meetings.sort(key=lambda x: x[1])
+        last_end_time = -1
+        count = 0
+        for meeting in meetings:
+            if meeting[0] > last_end_time:
+                count += 1
+                last_end_time = meeting[1]
+        return count
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        start = list(map(int,input().strip().split()))
+        end = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.maximumMeetings(n,start,end))
+# } Driver Code Ends
