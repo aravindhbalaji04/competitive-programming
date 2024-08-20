4312490b6bf0532997af1876ec25490223560663
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-08-20 23:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (49.89%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -3,10 +3,10 @@ public:
     string interpret(string command) {
         string result = "";
         for(int i=0; i<command.size(); ++i){
-            if(command[i]=='(' || command[i]==')'){
-                if(command[i+1]==')'){
-                    result.push_back('o');
-                }
+            if(command[i]=='(' && command[i+1]==')'){
+                result.push_back('o');
+            }else if (command[i]=='(' || command[i]==')'){
+                continue;
             }else{
                 result.push_back(command[i]);
             }
