bd5d67a8d9b495a83177480733ec28c40e2ab836
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-09-03 14:06:53 +0530

    Time: 3 ms (42.49%) | Memory: 7.7 MB (74.50%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int ans=0, bal=0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '('){
+                bal += 1;
+            }else{
+                bal -= 1;
+            }    
+            if(bal == -1){
+                bal += 1;
+                ans += 1;
+            }
+        }
+        return ans+bal;
+    }
+};
