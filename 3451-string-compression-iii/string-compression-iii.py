ec736a74882a7511f19d24bf1da73e13e956e695
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-11-04 11:03:06 +0530

    Time: 211 ms (72.34%) | Memory: 18.9 MB (90.27%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def compressedString(self, word: str) -> str:
+        comp = ""
+        i, n = 0, len(word)
+        while i < n:
+            char = word[i]
+            count = 0
+            while i < n and word[i] == char and count < 9:
+                count += 1
+                i += 1
+            comp += str(count) + char
+        return comp
