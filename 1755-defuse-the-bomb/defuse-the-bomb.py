5b2a27751d01a677225151ead74ce13266ff84ba
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-11-18 21:09:10 +0530

    Time: 7 ms (38.94%) | Memory: 16.7 MB (22.92%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.py
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -2,28 +2,14 @@ class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
         ans = []
         n = len(code)
+        extended = code*2
         if k == 0:
-            for i in range(n):
-                ans.append(0)
+            return [0]*n
         elif k > 0:
             for i in range(n):
-                val, a, b = 0, 0, i+1
-                while a < k:
-                    if b == len(code):
-                        b = 0
-                    val += code[b]
-                    a += 1
-                    b += 1
-                ans.append(val)
+                ans.append(sum(extended[i+1:i+k+1]))
         else:
-            k *= -1
             for i in range(n):
-                val, a, b = 0, 0, i-1
-                while a < k:
-                    if b == -1:
-                        b = len(code)-1
-                    val += code[b]
-                    a += 1
-                    b -= 1
-                ans.append(val)
+                print(extended[i-1:i-k])
+                ans.append(sum(extended[i+n+k:i+n]))
         return ans
